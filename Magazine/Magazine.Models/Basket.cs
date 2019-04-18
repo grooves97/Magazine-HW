0290d0cdeb8c237654682389e3a590adee99ea78
Аслан Нурсеитов <grooves97@gmail.com>
2019-04-18 11:54:06 +0600

    Создал модели и библиотеки

---
+++ Magazine/Magazine.Models/Basket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazine.Models
+{
+    public class Basket : Entity
+    {
+
+    }
+}
